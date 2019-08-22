c1e4ea6080c0e66740fffa93f14e86cd4dfc2eb8
Pietro Ventura <pietro.ventura@par-tec.it>
2019-08-22 16:11:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Getting Started
+Quick spring boot example.
+
+```
+mvn spring-boot:run
+```
+
+```
+curl localhost:8080/hello
+```
---
+++ src/main/java/com/github/pv/springbootms/HelloController.java
@@ -0,0 +1,16 @@
+package com.github.pv.springbootms;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+        return String.format("Hello %s", name);
+    }
+}
